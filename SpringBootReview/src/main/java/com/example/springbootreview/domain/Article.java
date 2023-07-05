44cc621a5cef564a00d57074c91a9e101d85b573
Jasper Oh <ohyj0906@gmail.com>
2023-07-04 21:11:49 -0800

    Refactor the code by using lombok annotation

--- SpringBootReview/src/main/java/com/example/springbootreview/domain/Article.java
+++ SpringBootReview/src/main/java/com/example/springbootreview/domain/Article.java
@@ -1,9 +1,14 @@
 package com.example.springbootreview.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
     @Id
@@ -39,24 +44,4 @@ public class Article {
         this.content = content;
     }
 
-    protected Article(){
-
-    }
-
-    /**
-     * Getters
-     *
-     * @return id , title, content
-     */
-    public Long getId(){
-        return id;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public String getContent(){
-        return content;
-    }
 }
