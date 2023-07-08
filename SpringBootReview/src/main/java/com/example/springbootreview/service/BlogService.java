bae69485b4cee0acb7b7ef2b74982acf2f00224e
Jasper Oh <ohyj0906@gmail.com>
2023-07-08 09:20:23 -0800

    Write the description-1

--- SpringBootReview/src/main/java/com/example/springbootreview/service/BlogService.java
+++ SpringBootReview/src/main/java/com/example/springbootreview/service/BlogService.java
@@ -6,13 +6,19 @@ import com.example.springbootreview.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
+@RequiredArgsConstructor // Add the constructor field that contain the final
+@Service //Register to Bean
 public class BlogService {
 
     private final BlogRepository blogRepository;
 
 
+    /**
+     * Method that
+     * Write the Blog article.
+     * @param request
+     * @return
+     */
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
     }
